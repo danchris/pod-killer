1475905cec3aa0038b7b1d9dbbaca8c4c0229e46
Daniel Christodoulopoulos <danielchristodoulopoulos@gmail.com>
2023-08-16 20:13:11 +0300

    Added main functionality Delete unused pods automatically

--- go.mod
+++ go.mod
@@ -2,7 +2,11 @@ module github.com/danchris/pod-killer
 
 go 1.20
 
-require k8s.io/client-go v0.27.4
+require (
+	k8s.io/api v0.27.4
+	k8s.io/apimachinery v0.27.4
+	k8s.io/client-go v0.27.4
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
@@ -36,8 +40,6 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/api v0.27.4 // indirect
-	k8s.io/apimachinery v0.27.4 // indirect
 	k8s.io/klog/v2 v2.90.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20230501164219-8b0f38b5fd1f // indirect
 	k8s.io/utils v0.0.0-20230209194617-a36077c30491 // indirect
--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"path/filepath"
+	"strconv"
 
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
@@ -15,36 +20,77 @@ func main() {
 		os.Getenv("HOME"), ".kube", "config",
 	)
 
-	// Initialize kubernetes-client
 	cfg, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
 	if err != nil {
 		fmt.Printf("Error building kubeconfig: %v\n", err)
 		os.Exit(1)
 	}
 
-	// create new client with the given config
-	// https://pkg.go.dev/k8s.io/client-go/kubernetes?tab=doc#NewForConfig
 	kubeClient, err := kubernetes.NewForConfig(cfg)
 	if err != nil {
 		fmt.Printf("Error building kubernetes clientset: %v\n", err)
 		os.Exit(2)
 	}
 
-	// use the app's label selector name. Remember this should match with
-	// the deployment selector's matchLabels. Replace <APPNAME> with the
-	// name of your choice
+	parsedSelector, err := labels.Parse("pod-killer/alive, pod-killer/name")
+	if err != nil {
+		fmt.Printf("Error bad label: %v", err)
+	}
+
 	options := metav1.ListOptions{
-		LabelSelector: "pod-killer=1/2",
+		LabelSelector: parsedSelector.String(),
 	}
 
-	// get the pod list
-	// https://pkg.go.dev/k8s.io/client-go@v11.0.0+incompatible/kubernetes/typed/core/v1?tab=doc#PodInterface
-	podList, _ := kubeClient.CoreV1().Pods("default").List(options)
+	podList, _ := kubeClient.CoreV1().Pods("default").List(context.TODO(), options)
 
-	// List() returns a pointer to slice, derefernce it, before iterating
+	var toBeKilled []string
 	for _, podInfo := range (*podList).Items {
-		fmt.Printf("pods-name=%v\n", podInfo.Name)
-		fmt.Printf("pods-status=%v\n", podInfo.Status.Phase)
-		fmt.Printf("pods-condition=%v\n", podInfo.Status.Conditions)
+		name := podInfo.ObjectMeta.Labels["pod-killer/name"]
+		aliveLabel, err := strconv.Atoi(podInfo.ObjectMeta.Labels["pod-killer/alive"])
+		if err != nil {
+			fmt.Printf("Error to get pod-killer/alive label %v\n", err)
+		}
+		currentAlive := getCurrentAlive(podList, name)
+		toBeKilled = append(toBeKilled, pickCandidates(podList, name, currentAlive-aliveLabel)...)
 	}
+
+	for _, v := range toBeKilled {
+		err := kubeClient.CoreV1().Pods("default").Delete(context.TODO(), v, metav1.DeleteOptions{})
+		if err != nil {
+			fmt.Printf("Error to kill pod %v\n", err)
+		}
+	}
+}
+
+func getCurrentAlive(podList *v1.PodList, name string) int {
+
+	count := 0
+	for _, podInfo := range (*podList).Items {
+		if podInfo.ObjectMeta.Labels["pod-killer/name"] == name {
+			count++
+		}
+	}
+
+	return count
+}
+
+func pickCandidates(podList *v1.PodList, name string, haveToBeKilled int) []string {
+
+	if haveToBeKilled <= 0 {
+		return nil
+	}
+
+	candidatesNumber := 0
+	var candidates []string
+
+	for _, podInfo := range (*podList).Items {
+		if podInfo.ObjectMeta.Labels["pod-killer/name"] == name {
+			candidates = append(candidates, podInfo.Name)
+			candidatesNumber++
+		}
+		if haveToBeKilled == candidatesNumber {
+			break
+		}
+	}
+	return candidates
 }
